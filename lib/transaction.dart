532511d33c7d2d239ad1a4c7513c57672a8e763d
Matthew Gleich <matthewgleich@gmail.com>
2020-01-01 22:59:30 -0500

    Create Transaction Method

--- lib/transaction.dart
+++ lib/transaction.dart
@@ -1,5 +1,14 @@
+import 'package:flutter/foundation.dart';
+
 class Transaction {
-  String id;
-  String title;
-  double amount;
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+
+  Transaction(
+      {@required this.id,
+      @required this.title,
+      @required this.amount,
+      @required this.date});
 }
