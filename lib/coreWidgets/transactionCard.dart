a213c5728314986bb8a0a46470aa0c57fd407f9f
Matthew Gleich <matthewgleich@gmail.com>
2020-01-02 22:53:44 -0500

    Style price section of card

--- lib/coreWidgets/transactionCard.dart
+++ lib/coreWidgets/transactionCard.dart
@@ -12,15 +12,15 @@ class TranactionCard extends StatelessWidget {
       child: Row(
         children: <Widget>[
           Container(
-            child: Text(
-              tx.amount.toString()
+            margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.black, width: 2),
             ),
+            padding: EdgeInsets.all(10),
+            child: Text(tx.amount.toString()),
           ),
           Column(
-            children: <Widget>[
-              Text(tx.title),
-              Text(tx.date.toString())
-            ],
+            children: <Widget>[Text(tx.title), Text(tx.date.toString())],
           )
         ],
       ),
