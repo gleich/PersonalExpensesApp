a6f18423ef1a3ffff8baa1ab1779ae84ccba34b8
Matthew Gleich <matthewgleich@gmail.com>
2020-01-30 10:36:53 -0500

    Update chart

--- lib/transactions/widgets/chart.dart
+++ lib/transactions/widgets/chart.dart
@@ -1,6 +1,27 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+
+import '../models/transaction.dart';
 
 class Chart extends StatelessWidget {
+  final List<Transaction> recentTransactions;
+
+  Chart(this.recentTransactions);
+
+  List<Map<String, Object>> get groupedTransactionValues {
+    return List.generate(
+      7,
+      (index) {
+        final weekDay = DateTime.now().subtract(Duration(days: index));
+        double totalSum;
+        for (var i = 0; i < recentTransactions.length; i++) {
+          if (recentTransactions[i].date == )
+        }
+        return {'day': DateFormat.E(weekDay), 'amount': 9.99};
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
