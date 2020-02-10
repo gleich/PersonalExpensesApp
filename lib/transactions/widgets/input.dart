0b4bfa5c3d73632fcfbaa5b133f09619f568d2d9
Matthew Gleich <matthewgleich@gmail.com>
2020-02-09 23:30:56 -0500

    Deal with null vals

--- lib/transactions/widgets/input.dart
+++ lib/transactions/widgets/input.dart
@@ -15,9 +15,12 @@ class _TransactionInputState extends State<TransactionInput> {
   DateTime _selectedDate;
 
   void _submitData() {
+    if (amountController.text.isEmpty) {
+      return;
+    }
     final enteredTitle = titleController.text;
     final enteredAmount = double.parse(amountController.text);
-    if (enteredTitle.isEmpty || enteredAmount <= 0) {
+    if (enteredTitle.isEmpty || enteredAmount <= 0 || _selectedDate == null) {
       return;
     }
     widget.addTx(enteredTitle, enteredAmount);
