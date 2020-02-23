3ff6f37e3bdb4c1d14380b927a5d186a5fb2e473
Matthew Gleich <matthewgleich@gmail.com>
2020-02-22 21:51:45 -0500

    Scroll input

--- lib/transactions/widgets/input.dart
+++ lib/transactions/widgets/input.dart
@@ -49,73 +49,80 @@ class _TransactionInputState extends State<TransactionInput> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      elevation: 5,
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: <Widget>[
-            TextField(
-              controller: titleController,
-              onSubmitted: (_) => _submitData(),
-              decoration: InputDecoration(
-                fillColor: Colors.green,
-                labelText: "Transaction Title",
-                labelStyle: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
-              ),
-            ),
-            TextField(
-              controller: amountController,
-              keyboardType: TextInputType.number,
-              onSubmitted: (_) => _submitData(),
-              decoration: InputDecoration(
-                fillColor: Colors.green,
-                labelText: "Amount",
-                labelStyle: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
-              ),
-            ),
-            Container(
-              height: 60,
-              child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: Text(
-                      _selectedDate == null
-                          ? "No Date Chosen!"
-                          : "Picked Date: ${DateFormat.yMd().format(_selectedDate)}",
-                    ),
+    return SingleChildScrollView(
+      child: Card(
+        elevation: 5,
+        child: Container(
+          padding: EdgeInsets.only(
+            top: 10,
+            left: 10,
+            right: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: <Widget>[
+              TextField(
+                controller: titleController,
+                onSubmitted: (_) => _submitData(),
+                decoration: InputDecoration(
+                  fillColor: Colors.green,
+                  labelText: "Transaction Title",
+                  labelStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
                   ),
-                  SizedBox(width: 10),
-                  FlatButton(
-                    onPressed: _presentDatePicker,
-                    child: Text(
-                      "Choose Date",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.green,
+                ),
+              ),
+              TextField(
+                controller: amountController,
+                keyboardType: TextInputType.number,
+                onSubmitted: (_) => _submitData(),
+                decoration: InputDecoration(
+                  fillColor: Colors.green,
+                  labelText: "Amount",
+                  labelStyle: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+              Container(
+                height: 60,
+                child: Row(
+                  children: <Widget>[
+                    Expanded(
+                      child: Text(
+                        _selectedDate == null
+                            ? "No Date Chosen!"
+                            : "Picked Date: ${DateFormat.yMd().format(_selectedDate)}",
                       ),
                     ),
-                  )
-                ],
-              ),
-            ),
-            RaisedButton(
-              child: Text(
-                "Add Transaction",
-                style: TextStyle(
-                  fontSize: 15,
+                    SizedBox(width: 10),
+                    FlatButton(
+                      onPressed: _presentDatePicker,
+                      child: Text(
+                        "Choose Date",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.green,
+                        ),
+                      ),
+                    )
+                  ],
                 ),
               ),
-              onPressed: _submitData,
-            )
-          ],
+              RaisedButton(
+                child: Text(
+                  "Add Transaction",
+                  style: TextStyle(
+                    fontSize: 15,
+                  ),
+                ),
+                onPressed: _submitData,
+              )
+            ],
+          ),
         ),
       ),
     );
